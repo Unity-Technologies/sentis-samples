d714a8d5e449932e482a91473bf77e528911085d
Xavier Désilets <xavier.desilets@xavier.desilets-7X6M>
2025-09-24 08:56:16 -0400

    Code merge

---
+++ TextToSpeechSample/Assets/Src/Handlers/KokoroHandler.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.InferenceEngine.Samples.TTS.Handlers
+{
+    public class KokoroHandler: IDisposable
+    {
+        const string k_KokoroModelPath = "onnx/model.onnx";
+        const string k_VoicesFolderPath = "Voices/";
+
+        Model m_Model;
+        Worker m_Worker;
+        readonly BackendType m_BackendType;
+
+        public KokoroHandler(BackendType backendType = BackendType.GPUCompute, bool lazyLoadModel = true)
+        {
+            m_BackendType = backendType;
+
+            if (!lazyLoadModel)
+                LoadModelIfMissing();
+        }
+
+        void LoadModelIfMissing()
+        {
+            if (m_Model != null)
+                return;
+
+            var modelAsset = Resources.Load<ModelAsset>(k_KokoroModelPath);
+
+            m_Model = ModelLoader.Load(modelAsset);
+
+            m_Worker = new Worker(m_Model, m_BackendType);
+        }
+
+        public async Task<Tensor<float>> Execute(int[] inputIds, float speed, Voice voice)
+        {
+            using var inputIdsTensor = new Tensor<int>(new TensorShape(1, inputIds.Length), inputIds);
+            using var speedTensor = new Tensor<float>(new TensorShape(1), new[] { speed });
+            using var voiceTensor = await GetVoiceVector(inputIdsTensor, voice.Tensor);
+
+            return await Execute(inputIdsTensor, voiceTensor, speedTensor);
+        }
+
+        public async Task<Tensor<float>> Execute(Tensor<int> inputIdsTensor, Tensor<float> voiceTensor, Tensor<float> speedTensor)
+        {
+            LoadModelIfMissing();
+
+            m_Worker.Schedule(inputIdsTensor, voiceTensor, speedTensor);
+            using var result = m_Worker.PeekOutput() as Tensor<float>;
+            var output = await result.ReadbackAndCloneAsync();
+            return output;
+        }
+
+        public static List<Voice> GetVoices()
+        {
+            var voices = new List<Voice>();
+            var voiceFiles = System.IO.Directory.GetFiles(Path.Join(Application.dataPath,"Resources",k_VoicesFolderPath), "*.bin");
+
+            foreach (var file in voiceFiles)
+            {
+                var voiceData = System.IO.File.ReadAllBytes(file);
+
+                var voiceArray = new float[voiceData.Length / sizeof(float)];
+                Buffer.BlockCopy(voiceData, 0, voiceArray, 0, voiceData.Length);
+
+                var styleShape = new TensorShape(voiceArray.Length / 256, 1, 256);
+                var tensor = new Tensor<float>(styleShape, voiceArray);
+                var fileName = System.IO.Path.GetFileNameWithoutExtension(file);
+                var voice = new Voice(fileName, tensor);
+                voices.Add(voice);
+            }
+
+            return voices;
+        }
+
+        async Task<Tensor<float>> GetVoiceVector(Tensor<int> inputIds, Tensor<float> voice)
+        {
+            var graph = new FunctionalGraph();
+            var tokenInput = graph.AddInput<float>(voice.shape, "voice");
+            var output = tokenInput[inputIds.count];
+            graph.AddOutput(output, "output");
+            var model = graph.Compile();
+
+
+            using var worker = new Worker(model, m_BackendType);
+            worker.Schedule(voice);
+            using var result = worker.PeekOutput() as Tensor<float>;
+            return await result.ReadbackAndCloneAsync();
+        }
+
+        public void Dispose()
+        {
+            m_Worker?.Dispose();
+            m_Worker = null;
+        }
+
+        public class Voice: IDisposable
+        {
+            public string Name;
+            public Tensor<float> Tensor;
+
+            public Voice(string name, Tensor<float> data)
+            {
+                Name = name;
+                Tensor = data;
+            }
+            public void Dispose()
+            {
+                Tensor?.Dispose();
+            }
+        }
+    }
+}
---
+++ TextToSpeechSample/Assets/Src/State/AppSlice.cs
@@ -0,0 +1,14 @@
+using Unity.AppUI.Redux;
+using Unity.InferenceEngine.Samples.TTS.Handlers;
+
+namespace Unity.InferenceEngine.Samples.TTS.State
+{
+    public static class AppSlice
+    {
+        public const string Name = "AppSlice";
+        public const string SetInputText = Name + "/SetInputText";
+        public const string SetVoice = Name + "/SetVoice";
+        public const string SetSpeed = Name + "/SetSpeed";
+        public const string GenerateSpeech = Name + "/GenerateSpeech";
+    }
+}
