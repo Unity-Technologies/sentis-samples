7327365ed512e70d6ee0eb9705f64288583cbc8f
Xavier Désilets <xavier.desilets@xavier.desilets-7X6M>
2025-11-03 11:26:22 -0500

    Removing old tokenization pipeline

--- ChatSample/Assets/tokenization-pipeline/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.ML.Tokenization.Tests")]
-
-// Required for advanced mocking with Moq
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- ChatSample/Assets/tokenization-pipeline/Normalizers/INormalizer.cs
+++
@@ -1,7 +0,0 @@
-namespace Unity.ML.Tokenization.Normalizers
-{
-    public interface INormalizer
-    {
-        SubString Normalize(SubString input);
-    }
-}
